af77d8762333e5874b11c8bd380049c86e9a9a07
Marina Ustinovich <marishka0509@mail.ru>
2023-05-31 17:39:49 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import LoadingComponent from './components/Loading/Loading';
+import ErrorComponent from './components/Error/Error';
+import DataComponent from './components/Data/Data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <DataComponent />
+      <ErrorComponent />
+      <LoadingComponent />
     </div>
   );
 }
